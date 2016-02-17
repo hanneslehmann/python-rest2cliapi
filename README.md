4595e99c1f4969c17d80e6889d519883e69266c3
Hannes <hannes.lehmann@nterra.com>
2016-02-17 11:07:45 +0100

    Added user header check and rights management, improved response message

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Provide a Gateway: Server which receives JSON/REST request and translates it to 
 A quick an dirty build gateway in python to provide a REST Server (Bottle) which accepts POST request and interprets the URL to use it as a command to execute in the shell. Additionally standard arguments can be configured or given as a JSON body in the call. If developed further this would give a powerful custom and flexible API!
 
 __Attention__: there is no authentication / encryption whatsoever! There is no command validation and escaping, you could easily damage something! I suggest to use this in controlled environments only - run only as super-unprivileged user (a good idea to create a seperate one!) or within a secured container!
+
+This program should be used behing a reverse-proxy (like nginx) which can handle the authentication and pass the username as header in the request.
+
+You should not configure 0.0.0.0 as host listening IP.
+
+
+Example calls
+```bash
+curl -X POST -H "Content-Type: application/json" -d "{\"arguments\":[\"ping from host\"]}" http://localhost:8180/echo
+curl -X POST -H "Content-Type: application/json" -d "{\"arguments\":[\"-la\"]}" http://localhost:8180/ls
+```
--- rest2cliapi.py
+++ rest2cliapi.py
@@ -1,38 +1,53 @@
 
 # test:
-# curl -X POST -H "Content-Type: application/json"  http://localhost:8180/ls
-# curl -X POST -H "Content-Type: application/json" -d "{\"arguments\":[\"-la\"]}" http://localhost:8180/ls
-# curl -X POST -H "Content-Type: application/json" -d "{\"arguments\":[\"myfile.txt\"]}" http://localhost:8180/cat
-from bottle import route, run, template, request
+# curl -X POST -H "user: user1" -H "Content-Type: application/json"  http://localhost:8180/ls
+# curl -X POST -H "user: user1" -H "Content-Type: application/json" -d "{\"arguments\":[\"-la\"]}" http://localhost:8180/ls
+# curl -X POST -H "user: user1" -H "Content-Type: application/json" -d "{\"arguments\":[\"myfile.txt\"]}" http://localhost:8180/cat
+from bottle import route, run, template, request, response, abort
 import subprocess
 import json
 
+host="127.0.0.1"
+port=8180
+
 @route('/<command>', method='POST')
 def index(command):
+    resp = {"response":"","error":True,"message":"something went wrong!"}
     # read in configuration
     configuration = json.load(open("rest2cliapi.config.json"))
     if command in configuration:
+        response.content_type = 'application/json'
         cmd = configuration[command]["command"]
-        try:
-            req = json.load(request.body)
-            if "arguments" in req:
-                arguments = req["arguments"]
-        except:
-            arguments = configuration[command]["arguments"]
-        if len(arguments)>0:
-            cmd_arr = cmd.split(" ") + arguments
-        else:
-            cmd_arr = cmd.split(" ")
-        print cmd_arr
-        try:
-            if len(cmd_arr)>1:
-                result = subprocess.check_output(cmd_arr,stderr=subprocess.STDOUT)
+        # check if user is allowed for this action
+        if request.headers.get('user') in configuration[command]["user"]:
+            if configuration[command]["overwrite_arguments"]:
+                try:
+                    req = json.load(request.body)
+                    if "arguments" in req:
+                        arguments = req["arguments"]
+                except:
+                    arguments = configuration[command]["arguments"]
             else:
-                result = subprocess.check_output(cmd_arr[0],stderr=subprocess.STDOUT)
-        except:
-            result = '{"error": "an internal error occured"}'
-        return template('{"response": "'+ result +'"}')
+                arguments = ""
+            if len(arguments)>0:
+                cmd_arr = cmd.split(" ") + arguments
+            else:
+                cmd_arr = cmd.split(" ")
+            print cmd_arr
+            try:
+                if len(cmd_arr)>1:
+                    result = subprocess.check_output(cmd_arr,stderr=subprocess.STDOUT)
+                else:
+                    result = subprocess.check_output(cmd_arr[0],stderr=subprocess.STDOUT)
+                result = result.split("\n")
+            except:
+                result = '{"error": "an internal error occured"}'
+            resp["response"] = result
+            resp["error"] = False
+            resp["message"] = "All OK!"
+        else:
+            abort(401, "Sorry, access denied.")
     else:
-        return template('{"response": {"error":"No command found!"}}')
-
-run(host='localhost', port=8180)
+        resp["message"] = "Command not defined!"
+    return json.dumps(resp)
+run(host=host, port=port)
